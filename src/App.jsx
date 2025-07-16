788915a67b42a218aeed2c7ab957b1347af35f02
Salman-sajib <sfsajib@gmail.com>
2025-07-16 17:16:16 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Accordion from "./components/Accordion";
+
+function App() {
+  return (
+    <div className="bg-zinc-950 min-h-screen px-5 py-10 text-gray-50">
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Accordion.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import InterviewQuestions from "../data/interviewQuestions.json";
+import { motion, AnimatePresence } from "motion/react";
+
+const Accordion = () => {
+  const [openId, setOpenId] = useState(null);
+
+  const toggleAnswer = (id) => {
+    setOpenId(openId === id ? null : id);
+  };
+
+  return (
+    <div className="w-full max-w-[1450px] mx-auto">
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+        <h1 className="text-7xl md:text-9xl font-display">FAQ</h1>
+        <p className="font-light text-sm text-white flex items-center w-full max-w-[70ch] font-inter">
+          A collection of commonly asked React interview questions and answers,
+          presented in an interactive accordion format. This component is
+          designed to help developers quickly review key concepts, clarify
+          fundamental differences, and reinforce their understanding of React’s
+          core principles. Perfect for interview preparation or daily practice.
+        </p>
+      </div>
+
+      <div className="mt-[100px] md:mt-[200px] w-full max-w-[900px] ml-auto text-gray-950 space-y-6">
+        {InterviewQuestions.map((item) => {
+          const isOpen = openId === item.id;
+
+          return (
+            <div key={item.id} className="w-full bg-gray-100 p-5 rounded-sm">
+              <button
+                onClick={() => toggleAnswer(item.id)}
+                className="w-full flex items-center justify-between cursor-pointer"
+              >
+                <span className="text-3xl md:text-5xl font-inter font-thin">
+                  {item.question}
+                </span>
+                <motion.svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-plus-icon lucide-plus size-[32px] md:size-[46px]"
+                  animate={{ rotate: isOpen ? 180 : 0 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <motion.path d="M5 12h14" />
+                  <motion.path
+                    d="M12 5v14"
+                    animate={{ opacity: isOpen ? 0 : 1 }}
+                    transition={{ duration: 0.2 }}
+                  />
+                </motion.svg>
+              </button>
+
+              <AnimatePresence initial={false}>
+                {isOpen && (
+                  <motion.div
+                    initial={{ height: 0, opacity: 0 }}
+                    animate={{ height: "auto", opacity: 1 }}
+                    exit={{ height: 0, opacity: 0 }}
+                    transition={{
+                      height: { duration: 0.4, ease: "easeInOut" },
+                      opacity: { duration: 0.4, delay: 0.3 },
+                    }}
+                    className="overflow-hidden"
+                  >
+                    <p className="pt-3 max-w-[60ch] text-base md:text-lg font-light text-gray-600">
+                      {item.answer}
+                    </p>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
